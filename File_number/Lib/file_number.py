3620a8f4dc1de8bdf2c99cd202a7cf06981565f6
corson94 <jamie.corson.20@ucl.ac.uk>
2023-01-23 18:12:53 +0000

    Re-ran the condensing process to remove an error in previous files

--- File_number/Lib/file_number.py
+++ File_number/Lib/file_number.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import os
 
-file_number = pd.read_parquet('Lib/file_number.gzip')
-HH = int(input('Enter household number (1-5566): '))
+
+
+file_number = pd.read_parquet('file_number.gzip')
+HH = int(input('Enter houseHold number (1-5566): '))
 num = file_number.loc[file_number['LCLid'] == HH,'file_number'].iloc[0]
-print('Household number', HH, 'is in file', num)
+C_ToU = file_number['std(0) or ToU(1)'].loc[HH]
+#name = file_number.loc[file_number[''] == HH,'file_number'].iloc[0]
+print('Household number: ' + str(HH) + '/nFile number: ' + str(num) + '/n Control or ToU: ' + str(C_ToU))
