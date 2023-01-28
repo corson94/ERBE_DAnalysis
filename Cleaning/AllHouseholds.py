e6be2974a4e54203a7c697c244e70281e87dbd42
corson94 <jamie.corson.20@ucl.ac.uk>
2023-01-28 14:37:47 +0000

    Minor changes to speed up and to integrate with myApp.py

--- Cleaning/AllHouseholds.py
+++ Cleaning/AllHouseholds.py
@@ -9,6 +9,7 @@ from datetime import datetime
 
 from Model.Cleaning.Household import Household
 
+slider = False
 
 class AllHouseholds(Household):
     """Brings together data from all households within a 'group' by calling on the Household module in order to
@@ -16,6 +17,9 @@ class AllHouseholds(Household):
 
     Call in 'AllHouseholds('group') where 'group' can equal either 'Control' or 'ToU'. Then any of the following
     functions can be called to plot the relevant data."""
+
+    load_data_all = [False, False]
+    file_all = [(), ()]
 
     def __init__(self, group: 'str'):
         """Returns a number of lists containing parameters of each Household within a group specified by the 'group'
@@ -27,7 +31,7 @@ class AllHouseholds(Household):
         Maximum number of consecutive zero entries
         Total number of data points
 
-        Above 5 lists are stored in a dataframe
+        Above 5 lists are stored in a df
 
         ## Lists commented out as not in use: ##
         Time series without null values
@@ -35,7 +39,8 @@ class AllHouseholds(Household):
         households
         """
         self.group = group
-        assert self.group == 'C' or self.group == 'T'
+        assert self.group == 'Control' or self.group == 'ToU'
+        self.group_num = Household.groups.index(self.group)
 
         self.HId = []
         self.num_null = []
@@ -46,157 +51,191 @@ class AllHouseholds(Household):
         # self.time_series = []
         # self.index_series = []
 
-        Household.file[Household.groups.index(self.group)] = pd.read_parquet('Data/Working_Data/Matrix/Original/matrix' + self.group + '.gzip')
-        self.range_of_households = [int(column[2:]) for column in Household.file[Household.groups.index(self.group)].columns]
-        Household.load_data[Household.groups.index(self.group)] = True
+        if not Household.load_data[self.group_num]:
+            Household.file_hh[self.group_num] = pd.read_parquet(
+                r"C:\Users\corso\OneDrive - University College London"\
+                r"\Documents\ERBE CDT\Energy Data Analysis\Data"\
+                r"\Working_Data\Matrix\Original\matrix" +
+                self.group[0] + '.gzip'
+            )
+            Household.load_data[self.group_num] = True
 
-        for i in tqdm(self.range_of_households):
-            self.HId.append(i)
-            self.num_null.append(Household(i).number_of_nulls())
-            self.num_points.append(Household(i).number_of_data_points())
-            self.num_zeros.append(Household(i).number_of_zeros())
-            # self.time_series.append(Household(i).dataframe.dropna().index)
-            # self.index_series.append(Household(i).dataframe.reset_index().dropna().index)
-            try:
-                self.max_consec_nulls.append(max(Household(i).number_of_consec_nulls(0)))
-            except:
-                self.max_consec_nulls.append(0)
+        if not AllHouseholds.load_data_all[self.group_num]:
+            range_of_households =[
+                int(column[2:]) for column in Household.file_hh[Household.groups.index(self.group)].columns]
 
-            try:
-                self.max_consec_zeros.append(max(Household(i).number_of_consec_zeros(0)))
-            except:
-                self.max_consec_zeros.append(0)
-        df = pd.DataFrame({'HId': self.HId,
-                           'num_null': self.num_null,
-                           'consec_nulls': self.max_consec_nulls,
-                           'num_points': self.num_points,
-                           'num_zeros': self.num_zeros,
-                           'consec_zeros': self.max_consec_zeros})
-        df = df.loc[df.num_points != 0]
-        df['null_percent'] = round(100*df.num_null/(df.num_null+df.num_points), 1)
-        df['zeros_percent'] = round(100 * df.num_zeros / df.num_points, 1)
-        self.df = df
+            for i in tqdm(range_of_households):
+                self.HId.append(i)
+                self.num_null.append(Household(i).number_of_nulls())
+                self.num_points.append(Household(i).number_of_data_points())
+                self.num_zeros.append(Household(i).number_of_zeros())
+                # self.time_series.append(Household(i).df.dropna().index)
+                # self.index_series.append(Household(i).df.reset_index().dropna().index)
+                try:
+                    self.max_consec_nulls.append(max(Household(i).number_of_consec_nulls(0)))
+                except:
+                    self.max_consec_nulls.append(0)
 
-    def write_or_plot(self, fig, name: 'str', write=False):
-        if write:
-            if os.path.exists('Data/Figures/AllHouseholds/' + self.group):
+                try:
+                    self.max_consec_zeros.append(max(Household(i).number_of_consec_zeros(0)))
+                except:
+                    self.max_consec_zeros.append(0)
+
+            df = pd.DataFrame({'HId': self.HId,
+                               'num_null': self.num_null,
+                               'consec_nulls': self.max_consec_nulls,
+                               'num_points': self.num_points,
+                               'num_zeros': self.num_zeros,
+                               'consec_zeros': self.max_consec_zeros})
+            df = df.loc[df.num_points != 0]
+            df['null_percent'] = round(100*df.num_null/(df.num_null+df.num_points), 1)
+            df['zeros_percent'] = round(100 * df.num_zeros / df.num_points, 1)
+            AllHouseholds.file_all[self.group_num] = df
+
+            AllHouseholds.load_data_all[self.group_num] = True
+
+    def write_or_plot_ah(self, fig, name: 'str', write=False):
+        if write == None:
+            return fig
+        elif write:
+            if os.path.exists(r"C:\Users\corso\OneDrive - University College London\Documents\ERBE CDT"\
+                              r"\Energy Data Analysis\Data\Figures\AllHouseholds" + self.group):
                 pass
             else:
-                os.makedirs('Data/Figures/AllHouseholds/' + self.group)
+                os.makedirs(r"C:\Users\corso\OneDrive - University College London\Documents\ERBE CDT"\
+                              r"\Energy Data Analysis\Data\Figures\AllHouseholds" + self.group)
 
-            fig.write_html('Data/Figures/AllHouseholds/' + self.group + '/' + name + 'html')
+            fig.write_html(r"C:\Users\corso\OneDrive - University College London\Documents\ERBE CDT"\
+                              r"\Energy Data Analysis\Data\Figures\AllHouseholds" +
+                           self.group + '/' + name + '.html')
         else:
             fig.show(renderer='browser')
 
     def hist_of_nulls(self, number_of_bins=50, write=False):
         """Histogram of number of null entries."""
-        fig = px.histogram(self.df.num_null)
+        df = AllHouseholds.file_all[self.group_num]
+        fig = px.histogram(df.num_null)
         fig.layout.sliders = [dict(
             active=4,
             currentvalue={"prefix": "Bin size: "},
             pad={"t": 20},
             steps=[dict(label=i, method='restyle', args=['xbins.size', i]) for i in
-                   np.arange(0, max(self.df.num_null), round(max(self.df.num_null) / number_of_bins, -2))]
+                   np.arange(0, max(df.num_null), round(max(df.num_null) / number_of_bins, -2))]
         )]
-        self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
+        return self.write_or_plot_ah(fig, name=inspect.stack()[0][3], write=write)
 
     def bar_of_nulls(self, number_of_hh=200, write=False):
         """Bar chart showing the number of null values for each household in descending order.
         'number_of_HH' input represents the top x households that are displayed."""
-        df = self.df.sort_values(by='num_null', ascending=False).reset_index(drop=True)
-        fig = px.bar(df, x=self.df.index, y='num_null', hover_data=['HId', 'null_percent'],
-                     labels={'num_null': 'Number of null entries ', 'HId': 'Household Id number ',
+        df = AllHouseholds.file_all[self.group_num]\
+            .sort_values(by='num_null', ascending=False)\
+            .reset_index(drop=True)
+
+        fig = px.bar(df, x=df.index, y='num_null', hover_data=['HId', 'null_percent'],
+                     labels={'HId': 'Household Id number ', 'num_null': 'Number of null entries ',
+                             'consec_nulls': 'Maximum number of consecutive null entries',
                              'null_percent': 'Percentage of null entries', 'x': 'Index'})
         fig.update_layout(
             xaxis=dict(
                 rangeslider=dict(
-                    visible=True,
+                    visible=slider,
                     range=[0, number_of_hh]
                 ),
                 type="linear",
-                range=[0, number_of_hh]
+                range=[0, round(number_of_hh / 2, 0)]
             )
         )
-        self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
+        return self.write_or_plot_ah(fig, name=inspect.stack()[0][3], write=write)
 
     def bar_of_consec_nulls(self, number_of_hh=200, write=False):
         """Bar chart showing the maximum number of consecutive null values for each household in descending order.
                 'number_of_HH' input represents the top x households that are displayed."""
-        df = self.df.sort_values(by='consec_nulls', ascending=False).reset_index(drop=True)
-        fig = px.bar(df, x=self.df.index, y='consec_nulls', hover_data=['HId', 'num_null', 'null_percent'],
+        df = AllHouseholds.file_all[self.group_num]\
+            .sort_values(by='consec_nulls', ascending=False)\
+            .reset_index(drop=True)
+
+        fig = px.bar(df, x=df.index, y='consec_nulls', hover_data=['HId', 'num_null', 'null_percent'],
                      labels={'num_null': 'Number of null entries ', 'HId': 'Household Id number ',
                              'null_percent': 'Percentage of null entries', 'x': 'Index',
                              'consec_nulls': 'Maximum number of consecutive null entries'})
         fig.update_layout(
             xaxis=dict(
                 rangeslider=dict(
-                    visible=True,
+                    visible=slider,
                     range=[0, number_of_hh]
                 ),
                 type="linear",
                 range=[0, round(number_of_hh / 2, 0)]
             )
         )
-        self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
+        return self.write_or_plot_ah(fig, name=inspect.stack()[0][3], write=write)
 
     def bar_of_zeros(self, number_of_hh=200, write=False):
         """Bar chart showing the number of zero entries for each household in descending order.
                 'number_of_HH' input represents the top x households that are displayed."""
-        df = self.df.sort_values(by='num_zeros', ascending=False).reset_index(drop=True)
+        df = AllHouseholds.file_all[self.group_num].\
+            sort_values(by='num_zeros', ascending=False)\
+            .reset_index(drop=True)
+
         fig = px.bar(df, x=df.index, y='num_zeros', hover_data=['HId', 'zeros_percent'],
                      labels={'num_zeros': 'Number of zero entries ', 'HId': 'Household Id number ',
                              'zeros_percent': 'Percentage of zero entries', 'x': 'Index'})
         fig.update_layout(
             xaxis=dict(
                 rangeslider=dict(
-                    visible=True,
+                    visible=slider,
                     range=[0, number_of_hh]
                 ),
                 type="linear",
                 range=[0, round(number_of_hh / 2, 0)]
             )
         )
-        self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
+        return self.write_or_plot_ah(fig, name=inspect.stack()[0][3], write=write)
 
     def bar_of_consec_zeros(self, number_of_hh=200, write=False):
         """Bar chart showing the maximum number of consecutive zero entries for each household in descending order.
                         'number_of_HH' input represents the top x households that are displayed."""
-        df = self.df.sort_values(by='consec_zeros', ascending=False).reset_index(drop=True)
-        fig = px.bar(df, x=self.df.index, y='consec_zeros', hover_data=['HId', 'num_zeros', 'zeros_percent'],
+        df = AllHouseholds.file_all[self.group_num]\
+            .sort_values(by='consec_zeros', ascending=False)\
+            .reset_index(drop=True)
+
+        fig = px.bar(df, x=df.index, y='consec_zeros', hover_data=['HId', 'num_zeros', 'zeros_percent'],
                      labels={'num_zeros': 'Number of zero entries ', 'HId': 'Household Id number ',
                              'zeros_percent': 'Percentage of zero entries', 'x': 'Index',
-                             'consec_zeros': 'Maximum number of consecutive zero entries'},
-                     text='HId')
+                             'consec_zeros': 'Maximum number of consecutive zero entries'}
+                     )
         fig.update_layout(
             xaxis=dict(
                 rangeslider=dict(
-                    visible=True,
+                    visible=slider,
                     range=[0, number_of_hh]
                 ),
                 type="linear",
                 range=[0, round(number_of_hh / 2, 0)]
             )
         )
-        self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
+        return self.write_or_plot_ah(fig, name=inspect.stack()[0][3], write=write)
 
     def bar_of_zeros_percent(self, number_of_hh=200, write=False):
         """Bar chart showing the percentage of zero entries for each household in descending order.
                       'number_of_HH' input represents the top x households that are displayed."""
-        df = self.df.sort_values(by='zeros_percent', ascending=False).reset_index(drop=True)
+        df = AllHouseholds.file_all[self.group_num].sort_values(
+            by='zeros_percent', ascending=False
+        ).reset_index(drop=True)
         fig = px.bar(df, x=df.index, y='zeros_percent', hover_data=['HId', 'num_zeros'],
                      labels={'num_zeros': 'Number of zero entries ', 'HId': 'Household Id number ',
                              'zeros_percent': 'Percentage of zero entries', 'x': 'Index'})
         fig.update_layout(
             xaxis=dict(
                 rangeslider=dict(
-                    visible=True,
+                    visible=slider,
                     range=[0, number_of_hh]
                 ),
                 type="linear",
                 range=[0, round(number_of_hh / 2, 0)]
             )
         )
-        self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
+        return self.write_or_plot_ah(fig, name=inspect.stack()[0][3], write=write)
 
     def clean(self, threshold: ['int'], column: ['str']):
         """Function to clean the matrix dataset by excluding data in column x that is greater than some 'threshold'.
@@ -209,7 +248,7 @@ class AllHouseholds(Household):
         Total number of datapoints (including zeros, excluding NaNs): num_points
         """
 
-        matrix = pd.read_parquet('Data/Working_Data/Matrix/matrix' + self.group + '.gzip')
+        matrix = AllHouseholds.file_all[self.group_num]
 
         # if there is only one parameter and one threshold to use to clean
         if isinstance(threshold, int) and isinstance(column, str):
@@ -233,7 +272,8 @@ class AllHouseholds(Household):
 
         hh = ['HH' + str(i) for i in cleaned.HId]
         matrix_clean: object = matrix.loc[:, hh]
-        matrix_clean.to_parquet('Data/Working_Data/Matrix/Cleaned/matrix' +
+        matrix_clean.to_parquet(r"C:\Users\corso\OneDrive - University College London\Documents\ERBE CDT"\
+                                r"\Energy Data Analysis\Data\Working_Data\Matrix\Originalmatrix" +
                                 self.group + '_' +
                                 datetime.now().strftime('%H.%M_%d.%m.%Y') + '.gzip'
                                 )
--- Cleaning/Household.py
+++ Cleaning/Household.py
@@ -7,79 +7,84 @@ import inspect
 
 
 class Household:
-    """Module that takes the 'matrix' file and returns a dataframe of the chosen household only.
-    Then a number of functions can be applied to the household's dataframe to return a range of parameters and plots."""
-    groups = ['C', 'T']
-    file = [(), ()]
+    """Module that takes the 'matrix' file and returns a df of the chosen household only.
+    Then a number of functions can be applied to the household's df to return a range of parameters and plots."""
+
+    groups = ['Control', 'ToU']
+    file_hh = [(), ()]
     load_data = [False, False]
-    Id_range = [[0, 4442], [4443, 5565]]
-    file_number = pd.read_parquet('Data/Working_Data/Seperated_Files/File_number/Lib/file_number.gzip')
+    file_number = pd.read_parquet(r"C:\Users\corso\OneDrive - University College London\Documents\ERBE CDT" \
+                                  r"\Energy Data Analysis\Data\Working_Data\Seperated_Files\File_number\Lib" \
+                                  r"\file_number.gzip")
+
     number_of_Households = file_number.LCLid.iloc[-1]
-    range_of_CHouseholds = [0, file_number.LCLid.loc[file_number['std(0) or ToU(1)'] == 0].iloc[-1]+1]
-    range_of_THouseholds = [range_of_CHouseholds[1], number_of_Households+1]
+    range_of_Households = [[0, file_number.LCLid.loc[file_number['std(0) or ToU(1)'] == 0].iloc[-1] + 1],
+                           [file_number.LCLid.loc[file_number['std(0) or ToU(1)'] == 0].iloc[-1] + 1,
+                            number_of_Households]
+                           ]
 
     def __init__(self, Id):
         """Assign Household to variable using 'LCLid' number as Id."""
         assert isinstance(Id, int)
-        assert(0 <= Id <= 5565)
+        assert (0 <= Id <= 5565)
         self.Id = Id
 
-        if Household.Id_range[0][0] <= self.Id <= Household.Id_range[0][1]:
-            self.group = 0
+        if Household.range_of_Households[0][0] <= self.Id < Household.range_of_Households[0][1]:
+            self.group = Household.groups[0]
+            self.group_num = 0
         else:
-            self.group = 1
+            self.group = Household.groups[1]
+            self.group_num = 1
 
-        if not Household.load_data[self.group]:
-            Household.file[self.group] = pd.read_parquet('Data/Working_Data/Matrix/Original/matrix' + Household.groups[self.group] + '.gzip')
-            Household.load_data[self.group] = True
-        dataframe = Household.file[self.group]['HH' + str(self.Id)].loc[
-                    Household.file[self.group]['HH' + str(self.Id)].first_valid_index():
-                    Household.file[self.group]['HH' + str(self.Id)].last_valid_index()]
-        self.dataframe = dataframe
+        if not Household.load_data[self.group_num]:
+            Household.file_hh[self.group_num] = pd.read_parquet(r"C:\Users\corso\OneDrive - University College London"\
+                                                         r"\Documents\ERBE CDT\Energy Data Analysis\Data"\
+                                                         r"\Working_Data\Matrix\Original\matrix" +
+                                                         self.group[0] + '.gzip')
 
-        # elif 4443 <= self.Id <= 5565:
-        #     if not Household.load_dataT:
-        #         Household.fileT = pd.read_parquet('Data/Working_Data/Matrix/matrixT.gzip')
-        #         Household.load_dataT = True
-        #     dataframe = Household.fileT['HH' + str(self.Id)].loc[
-        #                 Household.fileT['HH' + str(self.Id)].first_valid_index():
-        #                 Household.fileT['HH' + str(self.Id)].last_valid_index()]
-        #     self.dataframe = dataframe
+            Household.load_data[self.group_num] = True
+
+        df = Household.file_hh[self.group_num]['HH' + str(self.Id)].loc[
+                    Household.file_hh[self.group_num]['HH' + str(self.Id)].first_valid_index():
+                    Household.file_hh[self.group_num]['HH' + str(self.Id)].last_valid_index()]
+        self.df = df
 
     def start_date(self):
         """Returns the timestamp of the first data point for this Household."""
-        return self.dataframe.index[0]
+        return self.df.index[0]
 
     def end_date(self):
         """Returns the timestamp of the last data point for this Household."""
-        return self.dataframe.index[-1]
+        return self.df.index[-1]
 
     def number_of_nulls(self):
         """Returns the number of NaNs for this Household."""
-        return self.dataframe.isna().sum()
+        return self.df.isna().sum()
 
-    def number_of_consec_nulls(self, minimum = 2):
-        """Returns a dataframe with the count of each set of consecutive NaNs, starting from sets of x where x is
+    def number_of_consec_nulls(self, minimum=2):
+        """Returns a df with the count of each set of consecutive NaNs, starting from sets of x where x is
         defined by the 'minimum' input"""
-        consec_nulls = self.dataframe.notna().cumsum()[self.dataframe.isna()].value_counts().reset_index(drop=True)
+        consec_nulls = self.df.notna().cumsum()[self.df.isna()].value_counts().reset_index(drop=True)
         return consec_nulls.loc[consec_nulls >= minimum]
 
     def number_of_data_points(self):
         """Returns the number of data points for this Household."""
-        return self.dataframe.count()
+        return self.df.count()
 
     def number_of_zeros(self):
         """Returns the number of zeros for this Household."""
-        return self.dataframe.loc[self.dataframe == 0].count()
+        return self.df.loc[self.df == 0].count()
 
-    def number_of_consec_zeros(self, minimum = 2):
-        """Returns a dataframe with the count of each set of consecutive zeros, starting from sets of x where x is
+    def number_of_consec_zeros(self, minimum=2):
+        """Returns a df with the count of each set of consecutive zeros, starting from sets of x where x is
         defined by the 'minimum' input"""
-        consec_zeros = (self.dataframe != 0).cumsum()[self.dataframe == 0].value_counts().reset_index(drop=True)
+        consec_zeros = (self.df != 0).cumsum()[self.df == 0].value_counts().reset_index(drop=True)
         return consec_zeros.loc[consec_zeros >= minimum]
 
     def write_or_plot(self, fig, name: 'str', write=False):
-        if write:
+        if write == None:
+            return fig
+        elif write:
             if os.path.exists('Data/Figures/Households/' + Household.groups[self.group]):
                 pass
             else:
@@ -92,22 +97,23 @@ class Household:
     def time_plot(self, write=False):
         """Returns a plot of time (y-axis) vs data point number (x-axis)."""
         fig = go.Figure()
-        fig.add_trace(go.Scatter(y=self.dataframe.dropna().index,line=dict(color='blue'), name=('H'+str(self.Id))))
+        fig.add_trace(go.Scatter(y=self.df.dropna().index, line=dict(color='blue'), name=('H' + str(self.Id))))
         fig.add_trace(go.Scatter(y=pd.date_range(self.start_date(), self.end_date(), freq='30min'),
                                  line=dict(color='red'), name='1:1 plot'))
-        fig.update_layout(title= (str(self.number_of_nulls()) +
-                                  ' null entries out of ' +
-                                  str(self.number_of_data_points() +
-                                      self.number_of_nulls()) +
-                                  ' total data points (' +
-                                  str(round(100*self.number_of_nulls()/(self.number_of_nulls()+self.number_of_data_points()),1)) +
-                                  '%)')
+        fig.update_layout(title=(str(self.number_of_nulls()) +
+                                 ' null entries out of ' +
+                                 str(self.number_of_data_points() +
+                                     self.number_of_nulls()) +
+                                 ' total data points (' +
+                                 str(round(100 * self.number_of_nulls() / (
+                                             self.number_of_nulls() + self.number_of_data_points()), 1)) +
+                                 '%)')
                           )
-        self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
+        return self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
 
     def kwh_plot(self, write=False):
         """Returns a plot of electricity consumption (kWh) vs time."""
-        fig = px.line(self.dataframe)
+        fig = px.line(self.df)
         fig.update_layout(
             xaxis=dict(
                 rangeslider=dict(
@@ -116,11 +122,11 @@ class Household:
                 type="date"
             )
         )
-        self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
+        return self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
 
     def frequency_plot(self, write=False):
         """Returns a frequency plot of electricity consumption (kWh)."""
-        fig = px.histogram(self.dataframe, labels={'variable': 'Household Id',
+        fig = px.histogram(self.df, labels={'variable': 'Household Id',
                                                    'count': 'Frequency',
                                                    'value': 'Electricity consumption (kWh)'})
         fig.layout.sliders = [dict(
@@ -128,8 +134,8 @@ class Household:
             currentvalue={"prefix": "Bin size: "},
             pad={"t": 20},
             steps=[dict(label=i, method='restyle', args=['xbins.size', i]) for i in np.linspace(0, 0.2, 21)]
-            )]
+        )]
         fig.update_layout(xaxis_title_text='Electricity consumption (kWh)',
-                          yaxis_title_text= 'Frequency',
-                          legend_title_text= 'Household Id')
-        self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
+                          yaxis_title_text='Frequency',
+                          legend_title_text='Household Id')
+        return self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
