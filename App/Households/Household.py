f9ba0e2122f9fa0edce4219c3a8fff3b07565af3
corson94 <jamie.corson.20@ucl.ac.uk>
2023-02-02 18:42:09 +0000

    Added function to group the data by different time periods, i.e. daily, weekly etc, while being able to use different functions on them, i.e. sum, mean etc. Also able to plot these

--- App/Households/Household.py
+++ App/Households/Household.py
@@ -39,33 +39,36 @@ class Household:
 
         if Household.file_hh[self.group_num] is None:
             Household.file_hh[self.group_num] = pd.read_parquet(rf"{Household.directory}\Working_Data\Matrix"
-                                                                rf"\Original\matrix{self.group[0]}.gzip")
-
-        df = Household.file_hh[self.group_num]['HH' + str(self.Id)].loc[
-                    Household.file_hh[self.group_num]['HH' + str(self.Id)].first_valid_index():
-                    Household.file_hh[self.group_num]['HH' + str(self.Id)].last_valid_index()]
-        self.df = df
+                                                                rf"\Different_formats\matrix_ymwd_{self.group[0]}.gzip")
+        df = Household.file_hh[self.group_num]
+        columns = list(df.columns[:4])
+        columns.append(f"HH{self.Id}")
+        df = df[columns].loc[
+             df[f"HH{self.Id}"].first_valid_index():
+             df[f"HH{self.Id}"].last_valid_index()]
+        df.rename(columns={f"HH{self.Id}": 'kwh'}, inplace=True)
+        self.datasets = {'hh_None': df}
 
     def start_date(self):
         """Returns the timestamp of the first data point for this Household."""
-        return self.df.index[0]
+        return self.datasets['hh_None'].index[0]
 
     def end_date(self):
         """Returns the timestamp of the last data point for this Household."""
-        return self.df.index[-1]
+        return self.datasets['hh_None'].index[-1]
 
     def number_of_nulls(self):
         """Returns the number of NaNs for this Household."""
-        return self.df.isna().sum()
+        return self.datasets['hh_None'].isna().sum()
 
     def nulls_percent(self):
         """Returns the number of NaNs for this Household as a percentage of the total data points."""
-        return round(100 * self.df.isna().sum() / (self.df.isna().sum() + self.df.count()), 1)
+        return round(100 * self.datasets['hh_None'].isna().sum() / (self.datasets['hh_None'].isna().sum() + self.datasets['hh_None'].count()), 1)
 
     def max_number_of_consec_nulls(self, minimum=2):
         """Returns a df with the count of each set of consecutive NaNs, starting from sets of x where x is
         defined by the 'minimum' input"""
-        consec_nulls = self.df.notna().cumsum()[self.df.isna()].value_counts().reset_index(drop=True)
+        consec_nulls = self.datasets['hh_None'].notna().cumsum()[self.datasets['hh_None'].isna()].value_counts().reset_index(drop=True)
         try:
             return max(consec_nulls.loc[consec_nulls >= minimum])
         except ValueError:
@@ -73,24 +76,40 @@ class Household:
 
     def number_of_data_points(self):
         """Returns the number of data points for this Household."""
-        return self.df.count()
+        return self.datasets['hh_None'].count()
 
     def number_of_zeros(self):
         """Returns the number of zeros for this Household."""
-        return self.df.loc[self.df == 0].count()
+        return self.datasets['hh_None'].loc[self.datasets['hh_None'] == 0].count()
 
     def zeros_percent(self):
         """Returns the number of zeros for this Household as a percentage of the total data points."""
-        return round(100 * self.df.loc[self.df == 0].count() / self.df.count(), 1)
+        return round(100 * self.datasets['hh_None'].loc[self.datasets['hh_None'] == 0].count() / self.datasets['hh_None'].count(), 1)
 
     def max_number_of_consec_zeros(self, minimum=2):
         """Returns a df with the count of each set of consecutive zeros, starting from sets of x where x is
         defined by the 'minimum' input"""
-        consec_zeros = (self.df != 0).cumsum()[self.df == 0].value_counts().reset_index(drop=True)
+        consec_zeros = (self.datasets['hh_None'] != 0).cumsum()[self.datasets['hh_None'] == 0].value_counts().reset_index(drop=True)
         try:
             return max(consec_zeros.loc[consec_zeros >= minimum])
         except ValueError:
             return 0
+
+    def agg_data(self, period: str, calculation=None):
+        period_format = {
+            'day': 'd',
+            'week': 'W',
+            'month': 'm'
+        }
+        if period == 'day':
+            df_new = getattr(self.datasets['hh_None'].groupby([period]), calculation)()
+        elif period != 'year':
+            grouped = getattr(self.datasets['hh_None'].groupby(['year', period])["kwh"], calculation)()
+            df_new = pd.DataFrame(data=grouped.values,
+                                  index=pd.to_datetime([f"{a}-{b}-0" for a, b in grouped.index],
+                                                       format=f"%Y-%{period_format[period]}-%w"),
+                                  columns=['kwh'])
+        self.datasets[f"{period}_{calculation}"] = df_new
 
     def write_or_plot(self, fig, name: 'str', write=False):
         if write is None:
@@ -105,10 +124,13 @@ class Household:
         else:
             fig.show(renderer='browser')
 
-    def time_plot(self, write=False):
+    def time_plot(self, period='hh', calculation=None, write=False):
         """Returns a plot of time (y-axis) vs data point number (x-axis)."""
+        if f"{period}_{calculation}" not in self.datasets:
+            self.agg_data(period, calculation)
+        df = self.datasets[f"{period}_{calculation}"]
         fig = go.Figure()
-        fig.add_trace(go.Scatter(y=self.df.dropna().index, line=dict(color='blue'), name=('H' + str(self.Id))))
+        fig.add_trace(go.Scatter(y=df.index, line=dict(color='blue'), name=('H' + str(self.Id))))
         fig.add_trace(go.Scatter(y=pd.date_range(self.start_date(), self.end_date(), freq='30min'),
                                  line=dict(color='red'), name='1:1 plot'))
         fig.update_layout(title=f"{self.number_of_nulls()} null entries out of "
@@ -117,22 +139,25 @@ class Household:
                                 f"{round(100 * self.number_of_nulls() / (self.number_of_nulls() + self.number_of_data_points()), 1)} %)")
         return self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
 
-    def kwh_plot(self, write=False):
+    def kwh_plot(self, period='hh', calculation=None, write=False):
         """Returns a plot of electricity consumption (kWh) vs time."""
-        fig = px.line(self.df)
+        if f"{period}_{calculation}" not in self.datasets:
+            self.agg_data(period, calculation)
+        df = self.datasets[f"{period}_{calculation}"]
+        fig = px.line(df, x=df.index, y="kwh")
         fig.update_layout(
-            xaxis=dict(
-                rangeslider=dict(
-                    visible=True
-                ),
-                type="date"
-            )
+            xaxis_title_text='Time',
+            yaxis_title_text='Electricity consumption (kWh)',
+            legend_title_text='Household Id'
         )
         return self.write_or_plot(fig, name=inspect.stack()[0][3], write=write)
 
-    def frequency_plot(self, write=False):
+    def frequency_plot(self, period: str = 'hh', calculation=None, write=False):
         """Returns a frequency plot of electricity consumption (kWh)."""
-        fig = px.histogram(self.df, labels={'variable': 'Household Id',
+        if f"{period}_{calculation}" not in self.datasets:
+            self.agg_data(period, calculation)
+        df = self.datasets[f"{period}_{calculation}"]
+        fig = px.histogram(df.kwh, labels={'variable': 'Household Id',
                                             'count': 'Frequency',
                                             'value': 'Electricity consumption (kWh)'})
         fig.layout.sliders = [dict(
